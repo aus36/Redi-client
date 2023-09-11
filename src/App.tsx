53cb760f9eb5166cc0f00701e7184de33eb3fc2b
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-09-10 19:32:41 -0500

    Added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import './App.css'
+import React, { useState } from 'react'
+import './styles/App.css'
 
 function App() {
 
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+
   return (
     <div className='main-container'>
-      <img src='./redilogo.png' alt='Redi Logo' className='logo' />
-      <p className='typewriter'>Hello! Welcome to Redi, your interview helper.</p>
+      <div className='header'>
+        <img src='./redilogo.png' alt='Redi Logo' className='logo' />
+        <p className='typewriter'>Hello! Welcome to Redi, your interview helper.</p>
+      </div>
+      
+      <div className='login-form'>
+        <form>
+          <label htmlFor='username'>Username</label>
+          <input type='text' id='username' name='username' />
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' name='password' />
+          <button type='submit'>Login</button>
+        </form>
+      </div>
     </div>
   )
 }
